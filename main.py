ddb2ea0b5672f04f863c06ae130218c0ff02ba4e
atleib <87762720+atleib@users.noreply.github.com>
2022-08-01 17:40:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-
+#!/usr/bin/python
+#
+# Paramiko
+#
+import paramiko
+import sys
+import subprocess
+#
+# we instantiate a new object referencing paramiko's SSHClient class
+#
+vm = paramiko.SSHClient()
+vm.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+vm.connect('192.168.115.103', username='osmanl', password='xxxxxx')
+#
+vmtransport = vm.get_transport()
+dest_addr = ('10.103.53.26', 22) #edited#
+local_addr = ('192.168.115.103', 22) #edited#
+vmchannel = vmtransport.open_channel("direct-tcpip", dest_addr, local_addr)
+#
+stdin, stdout, stderr = jhost.exec_command("show version | no-more") #edited#
+#
+print stdout.read() #edited#
+#
+jhost.close()
+vm.close()
+# End
