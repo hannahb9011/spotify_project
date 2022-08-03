4120f140e50c1b7bf7f0a04fed5152e0ac599209
Hannah Bradley <79071679+hannahb9011@users.noreply.github.com>
2022-08-03 18:10:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 # Our VM's information
 host = "129.114.25.206" # Public key
 username = "cc"
-password = " "
+password = "csgroup3"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
